a92f09fdea6b97819117dffb9e6b413828ead97d
Tyler Hsia <TylerHsia@gmail.com>
2021-03-26 09:21:07 -0700

    Chapter 9 done

--- app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
+++ app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
@@ -1,9 +1,12 @@
 package android.bignerdranch.com;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,6 +45,7 @@ public class CrimeListFragment extends Fragment {
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+        private ImageView mSolvedImageView;
         private Crime mCrime;
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -52,12 +56,14 @@ public class CrimeListFragment extends Fragment {
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
         }
 
         public void bind(Crime crime){
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         @Override
